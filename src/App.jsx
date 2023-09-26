0e58f5afc280c53309c2923010a26c4a8248583e
Alex Martinez <alexmartinez.mm98@gmail.com>
2023-09-26 17:52:56 -0500

    add vercel conf

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import { playMatchedSound, playFailedSound } from './utils'
 
 import { ScorePanel } from './components/ScorePanel'
